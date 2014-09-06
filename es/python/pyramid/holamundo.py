bf5e6a3172db78da253f4f8375197f52ce33b097
Sergio E <ark.sega@gmail.com>
2014-09-06 00:35:35 -0500

    Standard encoding
    With this changes your code will pass from -0.77 to 7.69 in pylint

--- es/python/pyramid/holamundo.py
+++ es/python/pyramid/holamundo.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
+'''Pyramid hello world'''
 
-# includes
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 
-# vista
-def hola_mundo( request ):
-    return Response( 'Hola %(susodicho)s!' % request.matchdict )
+def hola_mundo(request):
+    '''Vista'''
+    return Response('Hola %(susodicho)s!' % request.matchdict)
 
 # controlador
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route( 'hola', '/hola/{susodicho}' )
-    config.add_view( hola_mundo, route_name='hola' )
+    config.add_route('hola', '/hola/{susodicho}')
+    config.add_view(hola_mundo, route_name='hola')
 
     app = config.make_wsgi_app()
-    server = make_server( '0.0.0.0', 8080, app )
+    server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
+
